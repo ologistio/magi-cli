4d99a45619b1ddcf9a8a53c5d1d72c9d4d07e8e0
Josh Finch <me@joshfinch.com>
2024-03-17 03:23:46 +0000

    Adds minimum viable cookiecutter

--- src/magi_cli/__main__.py
+++ src/magi_cli/__main__.py
@@ -1,13 +1,28 @@
 """Command-line interface."""
 
 import click
+from cookiecutter.main import cookiecutter
 
 
-@click.command()
+@click.group()
 @click.version_option()
 def main() -> None:
     """Magi."""
 
 
-if __name__ == "__main__":
-    main(prog_name="magi")  # pragma: no cover
+@main.command()
+def new() -> None:
+    """Generate a new Magi project."""
+    cookiecutter(
+        # Replace with "get_repository_for_template" method that parses the
+        # .magi.yml and works out which repository a given template is stored in.
+        "git@github.com:ologistio/base.git",
+        # Replace with the template name
+        directory="new",
+        # Replace with "lockfile.new_version_available" to determine if the file
+        # has changed on the remote and meets the version requirements in .magi.yml
+        overwrite_if_exists=True,
+        # Capture input with something nicer than Cookiecutter? Or just keep
+        # it simple?
+        no_input=True,
+    )
